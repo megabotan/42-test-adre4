cf3fbdf316a8015e6c2522be6ddef495f8d9d9b2
adrekoval@gmail.com <adrekoval@gmail.com>
2013-04-16 14:01:54 +0300

    ticket:1 Fixed tests and removed fixture migration

--- django_hello_world/hello/tests/regular_tests.py
+++ django_hello_world/hello/tests/regular_tests.py
@@ -10,5 +10,13 @@ class HttpTest(TestCase):
     def test_home(self):
         response = self.client.get('/')
         self.assertContains(response, self.me.name)
+        self.assertContains(response, self.me.last_name)
+        self.assertContains(response,
+                            self.me.date_of_birth.strftime('%B %d, %Y'))
         for string in self.me.bio.splitlines():
+            self.assertContains(response, string)
+        self.assertContains(response, self.me.email)
+        self.assertContains(response, self.me.jabber)
+        self.assertContains(response, self.me.skype)
+        for string in self.me.other_contacts.splitlines():
             self.assertContains(response, string)
