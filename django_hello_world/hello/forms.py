a283e173220be24d4b84bc2b60653b77ef02f737
adrekoval@gmail.com <adrekoval@gmail.com>
2013-04-22 17:12:48 +0300

    ticket:6 Fixed javascript, added tests

--- django_hello_world/hello/forms.py
+++ django_hello_world/hello/forms.py
@@ -1,16 +1,15 @@
 from django import forms
 from django_hello_world.hello.models import Person
-from django.conf import settings
 
 
 class CalendarWidget(forms.TextInput):
     class Media:
         css = {
-            'all': ('/static/css/smoothness/jquery-ui-1.10.2.custom.css',)
+            'all': ('css/smoothness/jquery-ui-1.10.2.custom.css',)
         }
-        js = (settings.STATIC_URL + 'js/jquery-1.9.1.js',
-              settings.STATIC_URL + 'js/jquery-ui-1.10.2.custom.js',
-              settings.STATIC_URL + 'js/my_widget.js',
+        js = ('js/jquery-1.9.1.js',
+              'js/jquery-ui-1.10.2.custom.js',
+              'js/my_widget.js',
               )
 
 
--- django_hello_world/hello/static/js/base.js
+++ django_hello_world/hello/static/js/base.js
@@ -1,63 +1,61 @@
-$(document).ready(function() {
-        add_image_features();
-        $('#edit_form').ajaxForm({
-            dataType : 'json',
-            beforeSend: function () {
-                clear_errors();
-                block_form();
-                 
-            },
-            success: function (response) {
-                $('#result').html(response.result);
-                if(response.result == 'Error')
-                {
-                    errors = '';
-                    for (error_ in response.errors)
-                    {
-                        errors+=response.errors[error_];
-                        error_elem = '';
-                        for (problem in response.errors[error_])
-                        {
-                            error_elem +=response.errors[error_][problem];
-                        }
-                        $('#error_'+error_).html('<strong>' + error_elem + '</strong><br/>');
+$(document).ready(function () {
+    add_image_features();
+    $('#edit_form').ajaxForm({
+        dataType: 'json',
+        beforeSend: function () {
+            clear_errors();
+            block_form();
+
+        },
+        success: function (response) {
+            $('#result').html(response.result);
+            if (response.result == 'Error') {
+                errors = '';
+                for (error_ in response.errors) {
+                    errors += response.errors[error_];
+                    error_elem = '';
+                    for (problem in response.errors[error_]) {
+                        error_elem += response.errors[error_][problem];
                     }
-                    
+                    $('#error_' + error_).html('<strong>' + error_elem + '</strong><br/>');
                 }
-                unblock_form();
-            },
-            error: function(response) { 
-                    unblock_form(); 
-                    $('#result').html('Error: data not send');
+
             }
-        });
+            unblock_form();
+        },
+        error: function (response) {
+            unblock_form();
+            $('#result').html('Error: data not send');
+        }
     });
-    
+
+    function add_image_features() {
+        document.getElementById('id_photo').setAttribute("onchange", "readURL(this);")
+    };
+
+    function block_form() {
+        $("#edit_form :input").attr("disabled", "disabled");
+    };
+
+    function unblock_form() {
+        $("#edit_form :input").removeAttr("disabled");
+    };
+
+    function clear_errors() {
+        $(".fieldWrapper div").empty();
+
+    };
+
+});
+
 function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = function (e) {
             var image = document.getElementById('thumb');
-                image.setAttribute('src', e.target.result);
-                image.style.visibility = 'visible';
-            };
-            reader.readAsDataURL(input.files[0]);
-        }
+            image.setAttribute('src', e.target.result);
+            image.style.visibility = 'visible';
+        };
+        reader.readAsDataURL(input.files[0]);
     }
-
-function add_image_features(){
-    document.getElementById('id_photo').setAttribute("onchange","readURL(this);")
-    };
-    
-function block_form(){
-    $("#edit_form :input").attr("disabled", "disabled");
-};
-
-function unblock_form(){
-    $("#edit_form :input").removeAttr("disabled");
-};
-
-function clear_errors(){
-        $(".fieldWrapper div").empty();
-
-};
+}
